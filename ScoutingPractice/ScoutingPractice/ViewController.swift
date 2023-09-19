75b602d8d5552537062d699b1fa9922ac8ab1492
Araav Nayak <104335810+AraavNayak@users.noreply.github.com>
2023-09-19 12:36:48 -0700

    Firebase integration

---
+++ ScoutingPractice/ScoutingPractice/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  ScoutingPractice
+//
+//  Created by Araav Nayak on 8/17/23.
+//
+
+import UIKit
+//import FirebaseCore
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+    /*
+    @UIApplicationMain
+    class AppDelegate: UIResponder, UIApplicationDelegate {
+
+      var window: UIWindow?
+
+      func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+          [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+      }
+    }*/
+    
+    
+    
+    
+
+
+}
+
