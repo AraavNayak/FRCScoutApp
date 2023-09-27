21a33824730704e7471a71f28d1b11d71afe31ab
Araav Nayak <104335810+AraavNayak@users.noreply.github.com>
2023-09-27 10:36:29 -0700

    Added coop grid UI functionality

---
+++ ScoutingPractice/ScoutingPractice/Draft.swift
@@ -0,0 +1,43 @@
+//
+//  Draft.swift
+//  ScoutingApp
+//
+//  Created by Araav Nayak on 9/23/23.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+    
+
+
+/*
+ private var data: [Int] = Array(1...20)
+ private let colors: [Color] = [.red, .green, .blue, .yellow]
+ 
+ private let adaptiveColumns = [
+     GridItem(.adaptive(minimum: 170))
+ ]
+ 
+ var body: some View {
+     NavigationView {
+         ScrollView {
+             LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+                 ForEach(data, id: \.self) { number in
+                     ZStack {
+                         Rectangle()
+                             .frame(width: 170, height: 170)
+                             .foregroundColor(colors[number%4])
+                         Text("\(number)")
+                             .foregroundColor(.white)
+                             .font(.system(size: 80, weight: .medium, design: .rounded))
+                     }
+                 }
+             }
+         }
+         .padding()
+         .navigationTitle("Grid TItle")
+     }
+ }
+ */
---
+++ ScoutingPractice/ScoutingPractice/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  ScoutingPractice
+//
+//  Created by Araav Nayak on 8/17/23.
+//
+
+import UIKit
+//import FirebaseCore
+import SwiftUI
+import Foundation
+
+class ViewController: UIViewController {
+    
+//
+//    private var data: [Int] = Array(1...20)
+//    private let colors: [Color] = [.red, .green, .blue, .yellow]
+//
+//    private let adaptiveColumns = [
+//        GridItem(.adaptive(minimum: 170))
+//    ]
+//
+//    var body: UIView {
+//        NavigationView {
+//            ScrollView {
+//                LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+//                    ForEach(data, id: \.self) { number in
+//                        ZStack {
+//                            Rectangle()
+//                                .frame(width: 170, height: 170)
+//                                .foregroundColor(colors[number%4])
+//                            Text("\(number)")
+//                                .foregroundColor(.white)
+//                                .font(.system(size: 80, weight: .medium, design: .rounded))
+//                        }
+//                    }
+//                }
+//            }
+//            .padding()
+//            .navigationTitle("Grid TItle")
+//        }
+//    }
+//
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+  
+    
+
+    
+    /*
+    @UIApplicationMain
+    class AppDelegate: UIResponder, UIApplicationDelegate {
+
+      var window: UIWindow?
+
+      func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+          [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+      }
+    }*/
+    
+    
+    
+    
+
+
+}
+
